bff9031e42e94ffcac214a63a7e79fc98dd6d8cf
Avishka-42 <avishka4767@gmail.com>
2025-04-01 22:56:42 +0530

    branch5

--- first.py
+++ first.py
@@ -1,17 +1,17 @@
-simple intrest.
+1 simple intrest.
 
 p = int (input('enter the principal'))
 t = int (input('enter the time'))
 r = int (input('enter the rate'))
 print((p*t*r)/100)
 
-square of number.
+2 square of number.
 
 n = int(input('enter the no'))
 s = n*n
 print(s)
 
-arithemetic operators.
+3 arithemetic operators.
 
 a = int(input('enter the value of a'))
 b = int(input('enter the value of b'))
@@ -30,7 +30,7 @@ print(c)
 c = a**b
 print(c)
 
-assignment operator.
+4 assignment operator.
 
 a = int(input('enter the value of a'))
 a = a+5
@@ -42,5 +42,9 @@ print(a)
 a = a%5
 print(a)
 a = a||5
-print(a) 
+print(a)
+
+
+
+
 
